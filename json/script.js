2db9cabb77a18e4de976083c0870a27a3c3b8cac
thengjn <mustafaengn03@gmail.com>
2024-08-19 14:44:34 +0300

    navbar changed. dark mode fixed.

--- json/script.js
+++ json/script.js
@@ -2,8 +2,8 @@ const toggleButton = document.getElementById('theme-toggle');
   toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     if (document.body.classList.contains('dark-mode')) {
-      toggleButton.textContent = 'Açık Mod';
+      toggleButton.textContent = 'Light';
     } else {
-      toggleButton.textContent = 'Koyu Mod';
+      toggleButton.textContent = 'Dark';
     }
   });
